52c80250a61cce802fb36241beff9b6bef991de3
cag2050 <cag2050@163.com>
2017-03-14 00:06:00 +0800

    m

--- src/components/Watch.vue
+++ src/components/Watch.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    输入框内容发生变化，才会触发watch里面的函数
+    <h1><input v-model='msg'></h1>
   </div>
 </template>
 
@@ -9,11 +10,13 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'watch'
+      msg: 'watch3'
     }
   },
   watch: {
-
+    msg: function (val, oldVal) {
+        console.log('val = '+ val +' , oldVal = ' + oldVal)
+    }
   }
 }
 </script>
